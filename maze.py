a8df1bbd830c26969cff0a918ab0be0530c595a6
SisyphianLiger <69813075+SisyphianLiger@users.noreply.github.com>
2023-10-06 10:46:59 +0200

    Skeleton for Maze Class

--- maze.py
+++ maze.py
@@ -2,3 +2,12 @@ class Maze:
 
     def __init__(self):
         self
+
+    def _create_cells(self):
+        return
+
+    def _draw_cell(self, i, j):
+        return
+
+    def _animate(self):
+        return
